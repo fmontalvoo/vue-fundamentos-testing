53a1b3fda4bef1adc7b4755c330ff1156da747db
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-11-24 14:47:39 -0500

    Se crea componente Counter.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Counter />
+  <Counter :count="10" />
+  <Counter :count="100" />
+  <Counter :count="1000" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Counter,
   }
 }
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,59 @@
+<template>
+    <h2>Counter: {{ counter }}<sup>2</sup> = {{ exp }}</h2>
+    <div>
+        <button class="minus" @click="substract">-</button>
+        <button class="plus" v-on:click="add">+</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counter: this.count,
+        }
+    },
+    props: {
+        count: {
+            default: 1
+        }
+    },
+    methods: {
+        add() {
+            this.counter += 1
+        },
+        substract() {
+            this.counter -= 1
+        }
+    },
+    computed: {
+        exp() {
+            return this.counter * this.counter
+        }
+    }
+}
+</script>
+
+<style>
+.minus {
+    margin: 5px;
+    width: 30px;
+    height: 30px;
+    border-width: 1;
+    border-radius: 5px;
+    color: aliceblue;
+    border-color: crimson;
+    background-color: crimson;
+}
+
+.plus {
+    margin: 5px;
+    width: 30px;
+    height: 30px;
+    border-width: 1;
+    border-radius: 5px;
+    color: aliceblue;
+    border-color: green;
+    background-color: green;
+}
+</style>
