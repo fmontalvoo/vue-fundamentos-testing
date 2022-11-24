6fcf30f8b709a08fe1ebb8998e7f11b19e46650a
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-11-24 17:07:53 -0500

    Primer prueba.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,6 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
-
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+describe('Example', () => {
+  test('Value debe ser mayor a 10', () => {
+    const value = 20;
+    expect(value).toBeGreaterThan(10);
+  });
+});
