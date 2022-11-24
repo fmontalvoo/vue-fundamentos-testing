96b6ce4caa53bb8e3bfec31758cde2d9c410bae8
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-11-24 18:03:09 -0500

    Se simula "click" sobre elemento html.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- tests/unit/components/Counter.spec.js
+++ tests/unit/components/Counter.spec.js
@@ -24,4 +24,17 @@ describe('Counter.vue', () => {
         expect(h2.exists()).toBeTruthy();
         expect(h2.text()).toContain('Counter');
     });
+
+    test('El contador debe incrementar en 1 al hacer click en el boton "+"', async () => {
+        // Arrange
+        const h2 = component.find('h2');
+        const btnPlus = component.find('button.plus');
+
+        // Act
+        await btnPlus.trigger('click');
+
+        // Assert
+        expect(component.vm.counter).toEqual(2);
+        expect(h2.text()).toContain('4');
+    });
 });
