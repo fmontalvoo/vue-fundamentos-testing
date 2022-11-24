26a46bc22d801df4f54a5129c2e50ffce5a59213
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-11-24 17:40:05 -0500

    Se define un set de pruebas.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- tests/unit/components/Counter.spec.js
+++ tests/unit/components/Counter.spec.js
@@ -3,10 +3,25 @@ import { shallowMount, mount } from '@vue/test-utils';
 import Counter from '@/components/Counter.vue';
 
 describe('Counter.vue', () => {
+    let component;
+
+    beforeEach(() => {
+        component = shallowMount(Counter);
+    });
+
+    test('El componente Counter debe crearse', () => {
+        expect(component).toBeTruthy();
+        expect(component).toBeDefined();
+    });
+
     test('Debe hacer match con el snapshot', () => {
+        expect(component.html()).toMatchSnapshot();
+    });
 
-        const wrapper = shallowMount(Counter);
+    test('El elemento h2 debe tener el texto "Counter"', () => {
+        const h2 = component.find('h2');
 
-        expect(wrapper.html()).toMatchSnapshot();
+        expect(h2.exists()).toBeTruthy();
+        expect(h2.text()).toContain('Counter');
     });
 });
